41f700030db0078e3266a4fb634e804129ffa685
Eduardo Arias <eduarias@users.noreply.github.com>
2018-10-21 00:35:41 +0200

    fix issues with devices

--- darksky-api-device/app.py
+++ darksky-api-device/app.py
@@ -1,18 +1,25 @@
 import time
+import logging
 
 import requests
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)-8s %(threadName)-12s %(name)-12s: %(message)s',
+                    datefmt='%d/%m/%Y %H:%M:%S')
 
 
 def collect_data(device_name):
     response = requests.get('https://api.darksky.net/forecast/a8483764bba6a09c45c5ab2192baa671/41.390205,2.154007')
     if response.ok:
         current_weather_data = response.json()['currently']
-        res = {'name': device_name,
+        res = {'device_name': device_name,
                'data': {
                    'temperature': current_weather_data['temperature'],
                    'humidity': current_weather_data['humidity'] * 100,
                    'pressure': current_weather_data['pressure'],
                }}
+        logging.debug('Data to send: {}'.format(res))
         return res
 
 
@@ -20,5 +27,7 @@ if __name__ == '__main__':
     name = 'DarkSkyAPI'
     while True:
         r = requests.post('http://cloud_connector:8080/sensor/data',
-                          data=collect_data(name))
+                          json=collect_data(name))
+        logging.debug('Response: {} - {}'.format(r.status_code, r.text))
+        logging.debug('Data sent: {}'.format(r.request.body))
         time.sleep(60)
--- sense-hat-device/app.py
+++ sense-hat-device/app.py
@@ -1,13 +1,19 @@
 import time
+import logging
 
 import requests
 from sense_hat import SenseHat
+
+
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s %(levelname)-8s %(threadName)-12s %(name)-12s: %(message)s',
+                    datefmt='%d/%m/%Y %H:%M:%S')
 
 hat = SenseHat()
 
 
 def collect_data(device_name):
-    res = {'name': device_name,
+    res = {'device_name': device_name,
            'data': {
                'temperature': hat.get_temperature(),
                'humidity': hat.get_pressure(),
@@ -21,4 +27,6 @@ if __name__ == '__main__':
     while True:
         r = requests.post('http://cloud_connector:8080/sensor/data',
                           data=collect_data(name))
+        logging.debug('Response: {} - {}'.format(r.status_code, r.text))
+        logging.debug('Data sent: {}'.format(r.request.body))
         time.sleep(10)
